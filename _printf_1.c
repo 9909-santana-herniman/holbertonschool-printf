7260b1039968e18bb4dbbb0943824d1e866d1791
adashp02 <adashp02@gmail.com>
2025-03-25 02:12:09 +0000

    mini printf that handles chars, strings and %, tested with pedantic compiler

--- _printf_1.c
+++ _printf_1.c
@@ -17,20 +17,23 @@ int main(void)
 
 {
 	_printf("String:[%s]\n", "I am a string !");
+	_printf("Character:[%c]\n", 'H');
+	_printf("Percent:[%%]\n");
 	return (0);
 }
 
 int _printf(const char *format, ...)
 
 {
-	int length = 0; 
+	int length = 0;
+	int index = 0;	
 	/* counts the number of printed characters */
 	va_list args; 
 	/* list of arguments for the _printf variadic function */
 	va_start(args, format); 
 	/* starting position */
-
-	for (int index = 0; format[index] != '\0'; index++) 
+	
+	for (index = 0; format[index] != '\0'; index++)
 	/* going through the format array */
 
 	{
@@ -40,15 +43,23 @@ int _printf(const char *format, ...)
 		{
 			index++; 
 			/* keep moving to the next character */
+			
+			if (format[index] == '%')
 
-			if (format[index] == 's') 
+			{
+				putchar(format[index]);
+				length++;
+			}
+
+
+			else if (format[index] == 's') 
 			/* if we encounter 's' as in '%s' */
 
 			{
-				char *string; 
+				char *string;
+				int j;
 				string = va_arg(args, char * ); 
 				/* initializing string arg in list of args */
-				int j;
 
 				for (j = 0; string[j] != '\0'; j++) 
 				/* now moving along the characters in string */
@@ -62,6 +73,12 @@ int _printf(const char *format, ...)
 
 				}
 
+			}
+			else if (format[index] == 'c')
+
+			{
+				putchar(format[index]);
+				length++;
 			}
 
 		}
@@ -77,7 +94,8 @@ int _printf(const char *format, ...)
 			/* keep track of printed chars */
 
 		}
-
+		
+		
 
 	}
 
