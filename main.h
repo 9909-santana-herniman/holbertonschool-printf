2117dfbf19a3cdd47a4aa9b21460784429523a61
adashp02 <adashp02@gmail.com>
2025-03-27 08:16:54 +0000

    update

--- main.h
+++ main.h
@@ -6,6 +6,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * struct specif - specifiers
+ * @ptr: pointer to specifiers
+ * @f: functions for specifiers
+ */
+typedef struct specif
+{
+	char ptr;
+	int (*f)(va_list);
+} op_t;
+
+
 /* Functions to go here */
 int _printf(const char *format, ...);
 void print_number(int num);
