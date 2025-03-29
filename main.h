85acc5f0d635081303ee327af16c85ea562462aa
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-29 06:04:44 +0000

    Update

--- main.h
+++ main.h
@@ -14,5 +14,6 @@ void print_number(va_list args, int *count);
 void print_string(va_list args, int *count);
 void print_number(va_list args, int *count);
 void print_percent(int *count);
+void print_char(va_list args, int *count);
 
 #endif
