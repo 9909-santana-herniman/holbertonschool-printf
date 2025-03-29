3bb51e8702f988c1b1bd920c37328eaae070d844
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-29 04:35:24 +0000

    Updated

--- _printf.c
+++ _printf.c
@@ -13,13 +13,48 @@ int _printf(const char *format, ...)
 
 {
 	int len = 0; /* Keep track of printed characters */	
-	va_list args; /* List of arguments */ 
+	va_list args; /* List of arguments */
+       int i = 0;	
 
 	if (!format) /* Check for NULL format string */
 		return (-1);
 
 	va_start(args, format); /* Start processing arguments */
-	len = format_specifiers(format, args);
+	
+	/* Loop through each character in the format string */
+	while (format[i] != '\0')
+	{
+		/* If format specifier found */
+		if (format[i] == '%' && format[i +1] != '\0')
+		{
+			i++; /* Skip '%' */
+			switch (format[i]) /* Match the next character */
+			{
+				case 'c':
+					len += print_char(args);
+					break;
+				case 's':
+					len += print_string(args);
+					break;
+				case 'd':
+				case 'i':
+					len += print_number(args);
+					break;
+				case '%':
+					len += _putchar('%');
+					break;
+				default:
+					len += _putchar('%');
+					len += _putchar(format[i]);
+					break;
+			}
+		}
+		else
+		{
+			len =+ _putchar(format[i]);
+		}
+	i++;
+	}
 
 	va_end(args);
 
--- main.h
+++ main.h
@@ -2,30 +2,19 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
+#include <stdarg.h> /* for va_list and macros */
+#include <unistd.h> /* for write function */
 #include <stdlib.h>
 #include <stddef.h>
 
 /* Functions prototypes */
-int _printf(const char *format, ...);
-int format_specifiers(const char *format, va_list args);
+int _printf(const char *format, ...); /* substitue printf function */
+
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(va_list args);
 int print_number(va_list args);
+int print_number_helper(unsigned int num);
 int _putchar(char c);
-
-/**
- * struct specifier - Struct to link format specifiers to functions.
- * @specifier - The format specifier.
- * @f: The corresponding function pointer.
- */
-
-typedef struct specifier
-{
-	char form_spec;
-	int (*f)(va_list);
-}specifier_link;
 
 #endif
