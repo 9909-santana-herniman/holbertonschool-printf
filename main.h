67734f1b8604aaf500023f180e8865624def15ad
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-29 02:41:43 +0000

    Updated main.h

--- main.h
+++ main.h
@@ -5,27 +5,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <string.h>
-
-/**
- * struct specif - specifiers
- * @ptr: pointer to specifiers
- * @f: functions for specifiers
- */
-typedef struct specif
-{
-	char *ptr;
-	int (*f)(va_list);
-} op_t;
-
 
 /* Functions to go here */
 int _printf(const char *format, ...);
-void print_number(int num);
-int print_string(va_list args);
-int print_char(va_list args);
-int print_perc(va_list args);
-int (*specif_func(const char *format))(va_list);
+int print_number(int n);
+int print_string(char *str);
+int print_char(char c);
 
 #endif
