c76797e6d12367f68b3d58cce8e78e4a9b786bc0
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-29 03:27:12 +0000

    Updated helper func

--- helper_functions.c
+++ helper_functions.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer recursively
- * @num: The integer to print
+ * print_number - Prints an integer
+ * @args: List of arguments.
  * Return: Number of characters printed.
  */
 
-int  print_number(int n)
+int  print_number(va_list args)
 {
 	int count = 0;
 	unsigned int num; /* Holds the absolute value of num */
+	int n = va_arg(args, int);
 
 	/* Handle negative numbers */
 	if (n < 0)
@@ -28,21 +29,22 @@ int  print_number(int n)
 	}
 
 	/* Print the last digit */
-	count =+ putchar((num % 10) + '0');
+	count =+ _putchar((num % 10) + '0');
 
 	return (count);
 }
 
 /**
- * print_string - Prints string character by character.
- * @str: The input string to print.
- * Return: Number of characters printed
+ * print_string - Prints a string.
+ * @args: The list of arguments.
+ * Return: Number of characters printed.
  */
 
-int print_string(char *str)
+int print_string(va_list args)
 
 {
 	int count = 0;
+	char *str = va_arg(args, char *);
 
 	/* If the string is NULL, print "(null)" */
 	if (!str)
@@ -52,22 +54,43 @@ int print_string(char *str)
 
 	while (*str) /* Iterate through each character in the string */
 	{
-		count += putchar(*str); /* Print character */
+		_putchar(*str);
+		count++; /* Print character */
 		str++; /* Move to next character */
 	}
 	return (count);
 }
 
 /**
- * print_char - print single character.
- * @c: Character to print.
+ * print_char - Prints a single character.
+ * @args: List of arguments.
  * Return: 1 since char is printed.
  */
 
-int _print_char(char c)
-
+int _print_char(va_list args)
 {
-	/* Uses _putchar to print */
-	return putchar(c);
+	return (_putchar(va_arg(args, int)));
+}
 
+/**
+ * print_percent - Prints a percent sign (%)
+ * @args: Unused list of arguments.
+ * Return: Number of characters printed.
+ */
+
+int print_percent(va_list args)
+{
+	(void)args; /* unused parameter */
+	return (_putchar('%'));
+}
+
+/**
+ * _putchar - Prints a character to stdout
+ * @c: The character to print
+ * Return: 1 on success, -1 on error.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
